ebb8b557c850b184b94513ed4a62c9554f78d0b9
blqati <brahim98lqati@gmail.com>
2023-03-20 01:37:36 +0100

    add a second test to find bug, and update the readme

--- Tests/find_bug_test.py
+++ Tests/find_bug_test.py
@@ -1,5 +1,5 @@
 import unittest
-from selenium import webdriver
+from seleniumwire import webdriver
 import sys
 import os
 
@@ -14,6 +14,7 @@ class AdminPageTest(unittest.TestCase):
     
 
     def test_add_comment_with_empty_data(self):
+        """Add comment using empty data"""
         from Pages.admin_page import AdminPage
 
         adminePage = AdminPage(self.driver, self.url)
@@ -28,6 +29,37 @@ class AdminPageTest(unittest.TestCase):
         # as you see, i put a string in author field that contains just spaces
         # we should get errors
         self.assertTrue((adminePage.get_errors() is not None) and len(adminePage.get_errors() > 0))
+    
+    def test_add_valid_comment(self):
+        """ Test add comment action
+        The application send a request after a sucessful request to get comment
+        we will capture this request
+        """
+        from Pages.admin_page import AdminPage
+
+        adminePage = AdminPage(self.driver, self.url)
+        
+        # add comment
+        adminePage.enter_comment_author("author")
+        adminePage.enter_comment_content("content")
+
+        adminePage.add_comment()
+
+        add_comment_req = self.driver.wait_for_request('/api/poll/comment/*')
+
+        # verify status code
+        self.assertTrue(int(add_comment_req.response.status_code / 100) == 2)
+
+        # by default the application send after and add command, another request to get comments
+        # we capture the matching requests, and we check the status
+
+        for req in self.driver.requests:
+            # filter comment request
+            if '/comment'in req.path:
+                self.assertEquals(int(req.response.status_code / 100), 2)
+
+
+
 
     @classmethod
     def tearDownClass(cls):
--- exercises/findabug.md
+++ exercises/findabug.md
@@ -12,17 +12,38 @@ Include in this document the code of the test and, if you did it, the link to th
 
 ## Answer
 
+### Requirements
+- [Python](https://www.python.org/downloads/)
+
+You can install selenium and selenium-wire , using this command:
+```bash
+pip install selenium
+pip install selenium-wire
+```
+
 **<<Comments Part in the page (exo 2)>>**
-We have written a simple test that verifies that an error should be raised if we try to add an author name that contains an empty string. We have used the Page Object Model (POM) created earlier.
+We have written a simple test that verifies the creation of a new comment using the Page Object Model (POM) created earlier.
 
 To run the test, you need to modify the URL declared in the test [file](../Tests/find_bug_test.py) and set it to the link of the page requested in exercise 2.
 
 In my case, I tested with a local address linked to a DNS.
 
+
+##### First test:
+
+The first test is aimed at verifying whether attempting to add a comment with an empty author name will result in errors. 
+
+#### Second test:
+
+For the second test, I utilized the selenium-wire package, which enabled us to capture the requests that were sent. In the application, after adding a comment, a request is sent to retrieve the comments. However, an error 404 was encountered. Therefore, I conducted a test that captures this bug.
+
+
+#### Test run
 To execute the test, you need to navigate to the root of the project and run the command:
 
 ```bash
 python3 Tests/find_bug_test.py
 ```
+
 
 
